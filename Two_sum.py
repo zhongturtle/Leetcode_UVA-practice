4f413d23c67158a4777765b2b2f2ca0b26300dd7
Ziv <zhongturtle@gmail.com>
2019-01-23 23:47:10 +0800

    Create Two_sum.py
    feat: finish two sum problem
    
    use a dictionary to keep all the numbers of target vanish each number in the input array
    and check whether the number in the input array appear in the dictionary

---
+++ Two_sum.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        compDict = {}
+        
+        for x in range(len(nums)):
+            if nums[x] in compDict:
+                return [compDict[nums[x]],x]
+            compDict[target - nums[x]] = x
